c73ca96fff63073e56fa816cba3fe2d20c68d859
abraxas678 <abraxas678@gmail.com>
2022-05-25 10:57:01 +0200

    65

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ clear
 rm -rf $HOME/tmprestigrestore
 myspeed="2"
 #######################################################
-echo "version 64"; sleep $myspeed
+echo "version 65"; sleep $myspeed
 #######################################################
 cd $HOME
 ts=$(date +"%s")
