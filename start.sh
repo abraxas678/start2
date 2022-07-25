827e44e774425f2283890eb38ed320cecbb77035
abraxas678 <abraxas678@gmail.com>
2022-07-25 10:26:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ts=$(date +"%s")
 sudo rm -rf /tmp-restic-restore
 myspeed="2"
 #######################################################
-echo "version 115"; sleep $myspeed
+echo "version 116"; sleep $myspeed
 #######################################################
 cd $HOME
 ts=$(date +"%s")
