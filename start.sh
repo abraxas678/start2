b246642a1bc1f106bb684afedfa61e2fa0883a02
abraxas678 <abraxas678@gmail.com>
2022-07-29 05:30:50 +0200

    1

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ts=$(date +"%s")
 sudo rm -rf /tmp-restic-restore
 myspeed="2"
 #######################################################
-echo "version 132"; sleep $myspeed
+echo "version 133"; sleep $myspeed
 #######################################################
 cd $HOME
 ts=$(date +"%s")
