bdda4ac21a15f11d3c67193ce4aacd95141ce9b0
abraxas678 <abraxas678@gmail.com>
2022-08-16 20:41:01 +0200

    1

--- start.sh
+++ start.sh
@@ -147,17 +147,17 @@ x=0
 rm -f $HOME/tmp/pueuestatus.txt
 pueue-init
 
-while [[ $x -eq 0 ]]; do
-echo "PUEUE INIT"
-countdown 3
-export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
-/home/linuxbrew/.linuxbrew/bin/pueue status >>pueuestatus.txt 2>>pueuestatus.txt
-[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Failed to initialize client"* ]] &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
-[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Permission denied"* ]] && sudo chown -R abraxas: /run/user && sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueue &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
-[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Group"* ]] && x=1
-printf $HOME/tmp/pueuestatus.txt; cat $HOME/tmp/pueuestatus.txt
-sleep 1
-done
+#while [[ $x -eq 0 ]]; do
+#echo "PUEUE INIT"
+#countdown 3
+#export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
+#/home/linuxbrew/.linuxbrew/bin/pueue status >>pueuestatus.txt 2>>pueuestatus.txt
+#[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Failed to initialize client"* ]] &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
+#[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Permission denied"* ]] && sudo chown -R abraxas: /run/user && sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueue &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
+#[[ $(cat $HOME/tmp/pueuestatus.txt) = *"Group"* ]] && x=1
+#printf $HOME/tmp/pueuestatus.txt; cat $HOME/tmp/pueuestatus.txt
+#sleep 1
+#done
 rm -f $HOME/tmp/pueuestatus.txt
 /home/linuxbrew/.linuxbrew/bin/pueue status
 countdown 2
