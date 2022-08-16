726754625bf0a2a63a7dfd63527067413117507f
abraxas678 <abraxas678@gmail.com>
2022-08-16 19:42:34 +0200

    1

--- start.sh
+++ start.sh
@@ -145,13 +145,13 @@ echo; echo "PUEUE INSTALL"
 x=0
 rm -f pueuestatus.txt
 while [[ $x -eq 0 ]]; do
-echo --print "PUEUE INIT"
+echo "PUEUE INIT"
 countdown 3
 export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
-/home/linuxbrew/.linuxbrew/bin/pueue status >> pueuestatus.txt 2>> pueuestatus.txt
+/home/linuxbrew/.linuxbrew/bin/pueue status >>pueuestatus.txt 2>>pueuestatus.txt
 [[ $(cat pueuestatus.txt) = *"Failed to initialize client"* ]] &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
 [[ $(cat pueuestatus.txt) = *"Permission denied"* ]] && sudo chown -R abraxas: /run/user && sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueue &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
-[[ $(cat pueuestatus.txt) = *'Group "default"'* ]] && x=1
+[[ $(cat pueuestatus.txt) = *"Group"* ]] && x=1
 printf pueuestatus.txt; cat pueuestatus.txt
 sleep 1
 done
