459defd34714e9d43e6c4132cb53c21849d8cc8d
abraxas678 <abraxas678@gmail.com>
2022-08-16 20:39:20 +0200

    1

--- start.sh
+++ start.sh
@@ -36,6 +36,7 @@ pueue-init() {
    /home/linuxbrew/.linuxbrew/bin/pueue status
   [[ $(cat $HOME/tmp/pueuestatus.txt) = *"Failed to initialize client"* ]] &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
   [[ $(cat $HOME/tmp/pueuestatus.txt) = *"Permission denied"* ]] && sudo chown -R abraxas: /run/user && sudo chmod +x /home/linuxbrew/.linuxbrew/bin/pueue &&  /home/linuxbrew/.linuxbrew/bin/pueued -d && sleep 2 &&  /home/linuxbrew/.linuxbrew/bin/pueue status
+  [[ $(cat $HOME/tmp/pueuestatus.txt) = *"Please stop the daemon beforehand or delete the file manually"* ]] && rm -f /home/abraxas/.local/share/pueue/pueue.pid 
   [[ $(cat $HOME/tmp/pueuestatus.txt | head -n1) = *"Group"* ]] && x=1
   sleep 1
   done
