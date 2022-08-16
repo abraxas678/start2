37f94720d81ea0a5020fbc90ee2084a95a180676
abraxas678 <abraxas678@gmail.com>
2022-08-16 17:25:17 +0200

    1

--- new-disk.sh
+++ new-disk.sh
@@ -7,11 +7,10 @@ read -n 1 my_answer
 
  sudo mkdir /mnt/disk1
  sudo mkdir /mnt/disk1/home
- sudo mkdir /mnt/disk1/usr
 
- sudo mount -o discard,defaults /dev/sdb /mnt/disk1
+ sudo mount -o discard,defaults /dev/sdb /mnt/disk1/home
 
- sudo chmod a+w /mnt/disk1
+ sudo chmod a+w /mnt/disk1/home
 
  sudo cp /etc/fstab /etc/fstab-$ts.backup
 
@@ -21,9 +20,20 @@ read -n 1 my_answer
 
  ## --> UUID
 
- sudo echo "UUID=$UUID /mnt/disk1 ext4 discard,defaults,nofail 0 2" >> /etc/fstab
+ sudo echo "UUID=$UUID /home ext4 discard,defaults,nofail 0 2" >> /etc/fstab
+ echo "UUID=$UUID /home ext4 discard,defaults,nofail 0 2        in /etc/fstab"
+ echo BUTTON; read me
+ sudo nano /etc/fstab
 
  cat /etc/fstab
 
  echo "move /home to disk? (y/n)"
-[[ $my_answer = "y" ]] && cp -apx /home/* /mnt/newvar
+[[ $my_answer = "y" ]] && sudo cp -apx /home/* /mnt/disk1/home && sudo mv /home /home.old && sudo mkdir /home
+
+
+# https://www.suse.com/support/kb/doc/?id=000018399
+#Edit the /etc/fstab file:
+#Replace /mnt/newvar with /var
+#Save and Close the file.
+#Note: The new partition is now configured to be mounted at /var on boot.
+#Restart the server
