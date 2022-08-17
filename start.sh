1b78eaf3690661c634cc59d0095ac917a60d4ca4
abraxas678 <abraxas678@gmail.com>
2022-08-17 21:27:18 +0200

    1

--- start.sh
+++ start.sh
@@ -498,7 +498,7 @@ rclone copy gd:dotfiles/.p10k.zsh $HOME -P
 
 trenner SOFTWARE INSTALL
 countdown 2
-/home/linuxbrew/.linuxbrew/bin/pueue add -g system-setup -- sudo apt-get install restic SS -y
+/home/linuxbrew/.linuxbrew/bin/pueue add -g system-setup -- sudo apt-get install restic -y
 ###############################################################################  [6]
 echo
 trenner SSH SETUP
